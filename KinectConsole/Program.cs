45018e5e493056328f8ec1623ec130e6aa608f31
alexcercos <44820718+alexcercos@users.noreply.github.com>
2025-06-12 23:05:14 +0200

    Send skeleton info

--- KinectConsole/Program.cs
+++ KinectConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using Microsoft.Kinect;
 using System.Collections.Generic;
+using System.Text;
 
 namespace KinectConsole
 {
@@ -10,6 +11,10 @@ namespace KinectConsole
         KinectSensor _sensor;
         MultiSourceFrameReader _reader;
         IList<Body> _bodies;
+
+        IList<JointType> joints;
+
+        public string current_info;
 
         public void Initialize()
         {
@@ -22,6 +27,35 @@ namespace KinectConsole
                 _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
                 _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
             }
+
+            joints = new List<JointType> {
+                JointType.SpineBase,
+                JointType.SpineMid,
+                JointType.Neck,
+                JointType.Head,
+
+                JointType.ShoulderLeft,
+                JointType.ElbowLeft,
+                JointType.WristLeft,
+                JointType.HandLeft,
+
+                JointType.ShoulderRight,
+                JointType.ElbowRight,
+                JointType.WristRight,
+                JointType.HandRight,
+
+                JointType.HipLeft,
+                JointType.KneeLeft,
+                JointType.AnkleLeft,
+                JointType.FootLeft,
+
+                JointType.HipRight,
+                JointType.KneeRight,
+                JointType.AnkleRight,
+                JointType.FootRight
+
+                //Exclude hands
+            };
         }
 
         public void Deinitialize()
@@ -35,6 +69,11 @@ namespace KinectConsole
             {
                 _sensor.Close();
             }
+        }
+
+        public string GetCurrentInfo()
+        {
+            return current_info;
         }
 
         void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
@@ -58,13 +97,19 @@ namespace KinectConsole
                         if (!body.IsTracked)
                             continue;
 
-                        Console.WriteLine("BODY DETECTED");
-                        // Draw skeleton.
-                        //if (_displayBody)
-                        //{
-                        //    canvas.DrawSkeleton(body);
-                        //    _recorder.Update(body);
-                        //}
+                        StringBuilder jointPositions = new StringBuilder();
+
+                        foreach (JointType joint in joints)
+                        {
+                            var position = body.Joints[joint].Position;
+                            jointPositions.AppendFormat("{0:0.000},{1:0.000},{2:0.000},", position.X, position.Y, position.Z);
+                        }
+
+                        // Remove the trailing comma
+                        if (jointPositions.Length > 0)
+                            jointPositions.Length--;
+
+                        current_info = jointPositions.ToString();
                     }
                 }
             }
@@ -79,8 +124,6 @@ namespace KinectConsole
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting loop. Press Ctrl+C or send termination to stop.");
-
             // STARTUP
 
             kinect_program = new KinectProgram();
@@ -105,9 +148,8 @@ namespace KinectConsole
 
             while (keepRunning)
             {
-                Console.WriteLine("Test output");
-
-                Thread.Sleep(500);
+                Thread.Sleep(100);
+                Console.WriteLine(kinect_program.GetCurrentInfo());
             }
             
         }
--- call_programs.py
+++ call_programs.py
@@ -9,10 +9,25 @@ CREATE_NEW_PROCESS_GROUP = 0x00000200
 # Start process in its own process group
 process = subprocess.Popen(
     ["C:/Users/Alejandro/Documents/master/IOT/Final/KinectConsole/KinectConsole/bin/Release/KinectConsole.exe"],
-    creationflags=CREATE_NEW_PROCESS_GROUP
+    creationflags=CREATE_NEW_PROCESS_GROUP,
+    stdout=subprocess.PIPE,
+    text=True,            # Decode bytes to strings
+    bufsize=1             # Line-buffered
 )
 
-time.sleep(5)
+try:
+    while True:
+        i=0
+        for line in process.stdout:
+            print(f"Received ({i}):", line.strip())
+            i+=1
+        time.sleep(0.2)
 
-# Send CTRL_BREAK_EVENT (can only be sent to process groups)
-process.send_signal(signal.CTRL_BREAK_EVENT)
+except KeyboardInterrupt:
+    # Send CTRL_BREAK_EVENT (can only be sent to process groups)
+    process.send_signal(signal.CTRL_BREAK_EVENT)
+
+    for line in process.stdout:
+        print("Received (end):", line.strip())
+
+    print('Interrupted')
