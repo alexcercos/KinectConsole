8a8f49587323e85c7fa3762d47969e293cce74f6
alexcercos <44820718+alexcercos@users.noreply.github.com>
2025-06-18 23:38:33 +0200

    Send requests (simulate)

--- simulate_data.py
+++ simulate_data.py
@@ -2,6 +2,8 @@ import time
 import requests
 import pickle
 
+backend_url = "http://127.0.0.1:5000"
+
 def get_exercise():
     return "biceps_right"
 
@@ -19,8 +21,10 @@ for entry in complete_data:
 
     if data_type == "pox":
         print("P:",data_dict)
+        requests.post(f"{backend_url}/sendPox", json=data_dict)
     else:
         print("K;",data_dict)
+        requests.post(f"{backend_url}/sendKinect", json=data_dict)
 
     exercise = get_exercise()
     time.sleep(0.1) #arbitrary time
